c8d442999c14eb9c95be1888c890c3c8ccd3879a
hello form Lv-231.C++ <1111andrey1111@gmail.com>
2017-03-27 13:34:40 +0300

    finally deleted swap

--- anytype.cpp
+++ anytype.cpp
@@ -459,13 +459,6 @@ std::ostream& operator<<(std::ostream& output, const AnyType& obj)
 	return output;
 }
 
-/*void swap(AnyType& a, AnyType& b)
-{
-	using std::swap;
-	swap(a.value, b.value);
-	swap(a.selected_type, b.selected_type);
-}*/
-
 //disable warnings about some bad operations, because it's up to user
 #pragma warning(push)
 #pragma warning(disable:4800)
